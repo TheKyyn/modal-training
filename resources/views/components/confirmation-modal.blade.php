98532590792dedcdff6ec741c865c1e7eed3cf8c
Maxime Theophilos <maximet@MacBook-Air-de-Maxime.local>
2023-09-26 15:46:42 +0200

    View updated, users added into db

--- resources/views/components/confirmation-modal.blade.php
+++ resources/views/components/confirmation-modal.blade.php
@@ -1,19 +1,24 @@
-<div>
-    <div class="fixed inset-0 bg-gray-900 opacity-90"></div>
+<div {{ $attributes }}
+     x-data="{ show: @entangle($attributes->wire('model')) }"
+     x-show="show"
+     @keydown.escape.window="show = false"
+     style="display: none"
+>
+    <div class="fixed inset-0 bg-gray-900 opacity-90" @click="show = false"></div>
 
-    <div class="bg-white shadow-md p-4 max-w-sm h-40 m-auto rounded-md fixed inset-0">
+    <div class="bg-white shadow-md max-w-sm h-48 m-auto rounded-md fixed inset-0" x-show.transition="show">
         <div class="flex flex-col h-full justify-between">
-            <header>
+            <header class="p-6">
                 <h3 class="font-bold text-lg">
                     {{ $title }}
                 </h3>
             </header>
 
-            <main class="mb-4">
+            <main class="px-6 mb-4">
                 {{ $body }}
             </main>
 
-            <footer>
+            <footer class="flex justify-end px-6 py-4 mt-6 bg-gray-200 rounded-b-md">
                 {{ $footer }}
             </footer>
         </div>
--- resources/views/livewire/delete-user.blade.php
+++ resources/views/livewire/delete-user.blade.php
@@ -5,20 +5,18 @@
     </p>
 
 
-    @if ($showModal)
-        <x-confirmation-modal>
-            <x-slot name="title">
-                Êtes-vous sûr ?
-            </x-slot>
 
-            <x-slot name="body">
-                Si vous confirmez, votre compte sera supprimé définitivement.
-            </x-slot>
+    <x-confirmation-modal wire:model.defer="showModal">
+        <x-slot name="title">
+            Êtes-vous sûr ?
+        </x-slot>
+        <x-slot name="body">
+            Si vous confirmez, votre compte sera supprimé définitivement.
+        </x-slot>
+        <x-slot name="footer">
+            <x-button class="bg-gray-400 hover:bg-gray-500" wire:click="$set('showModal', false) ">Annuler</x-button>
+            <x-button class="bg-blue-400 hover:bg-blue-500" wire:click="handle">Confirmer</x-button>
+        </x-slot>
+    </x-confirmation-modal>
 
-            <x-slot name="footer">
-                <x-button class="bg-gray-400 hover:bg-gray-500" wire:click="$set('showModal', false) ">Annuler</x-button>
-                <x-button class="bg-blue-400 hover:bg-blue-500" wire:click="handle">Confirmer</x-button>
-            </x-slot>
-        </x-confirmation-modal>
-    @endif
 </form>
